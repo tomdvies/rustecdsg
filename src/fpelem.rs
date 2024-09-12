678d8c30998dbd8b035f7d5b97763842d3206ed8
Tom <davies.evan.tom@gmail.com>
2024-09-12 14:08:35 +0100

    fpstruct finished & removed U512 dep

--- src/fpelem.rs
+++ src/fpelem.rs
@@ -1,19 +1,195 @@
-use std::ops::{Add, Mul, Sub};
-extern crate primitive_types;
-use primitive_types::U512;
+use std::fmt::Debug;
+use std::ops::{Add, BitAnd, Div, Mul, Rem, Shr, Sub};
+//extern crate primitive_types;
+//use primitive_types::U512;
 
-#[derive(Debug, PartialEq, Clone, Copy)]
-pub struct FpElem {
-    pub number: U512,
-    pub prime: U512,
+pub trait GenericUInt:
+    Add<Output = Self>
+    + Sub<Output = Self>
+    + Mul<Output = Self>
+    + Rem<Output = Self>
+    + Copy
+    + BitAnd<Output = Self>
+    + PartialOrd
+    + PartialEq
+    + Shr<Output = Self>
+    + Default
+    + From<u8>
+{
 }
 
-impl FpElem {
-    pub fn new<T: Into<U512>, R: Into<U512>>(number: T, prime: R) -> Self {
-        let (prime, mut number): (U512, U512) = (prime.into(), number.into());
-        number = number % prime;
+impl<T> GenericUInt for T where
+    T: Add<Output = Self>
+        + Sub<Output = Self>
+        + Mul<Output = Self>
+        + Rem<Output = Self>
+        + Copy
+        + BitAnd<Output = Self>
+        + PartialOrd
+        + PartialEq
+        + Shr<Output = Self>
+        + Default
+        + From<u8>
+{
+}
+
+// https://www.jjj.de/fxt/fxtbook.pdf
+fn sub_mod<T: GenericUInt>(a: T, b: T, m: T) -> T {
+    let (a, b) = (a % m, b % m);
+    if a >= b {
+        return a - b;
+    } else {
+        return m - b + a;
+    }
+}
+
+fn add_mod<T: GenericUInt>(a: T, b: T, m: T) -> T {
+    let (a, b) = (a % m, b % m);
+    if b == T::from(0) {
+        a
+    } else {
+        let b = m - b;
+        sub_mod(a, b, m)
+    }
+}
+
+// Treat multiplication as composition of addiiton and spam binary expansion
+// See UofCambridge Quantum Information and Computation sheet 3 lol
+fn mul_mod<T: GenericUInt>(a: T, b: T, m: T) -> T {
+    let mut b = b;
+    let zero = T::from(0);
+    let mut result = zero;
+    let mut current = a;
+    while b != zero {
+        if (b & T::from(1)) != zero {
+            // note that the overflow safe property hinges on add_mod being overflow safe
+            result = add_mod(result, current, m);
+        }
+        current = add_mod(current, current, m);
+        b = b >> T::from(1);
+    }
+    result
+}
+
+// again spam binary expansion - quite slow as mul_mod does the same
+pub fn pow_mod<T: GenericUInt>(a: T, e: T, m: T) -> T {
+    let mut b = e;
+    let zero = T::from(0);
+    let one = T::from(1);
+    let mut result = one;
+    let mut current = a;
+    while b != zero {
+        if (b & T::from(1)) != zero {
+            // note that the overflow safe property hinges on add_mod being overflow safe
+            result = mul_mod(result, current, m);
+        }
+        current = mul_mod(current, current, m);
+        b = b >> T::from(1);
+    }
+    result
+}
+
+// this is slow, FLT
+fn mod_inv<T: GenericUInt>(a: T, m: T) -> T {
+    pow_mod(a, m - T::from(2), m)
+}
+
+// An element of the unique finite field of order p where p is prime
+// No checks are done for primality of p as this is expensive, however if p were to be a composite
+// number everything should work other than the Div trait (which will break even if m if invertible
+// mod p as mod_inv uses FLT)
+#[derive(Debug, PartialEq, Clone, Copy)]
+pub struct FpElem<T> {
+    pub number: T,
+    pub prime: T,
+}
+
+impl<T: GenericUInt> FpElem<T> {
+    pub fn new(number: T, prime: T) -> Self {
+        let number = number % prime;
+        FpElem { number, prime }
+    }
+
+    pub fn new_from(number: impl Into<T>, prime: impl Into<T>) -> Self {
+        let (number, prime) = (number.into(), prime.into());
+        let number = number % prime;
         FpElem { number, prime }
     }
 }
 
+//impl FpElem<U512> {
+//    pub fn new_u512(number: impl Into<U512>, prime: impl Into<U512>) -> Self {
+//        let (number, prime) = (number.into(), prime.into());
+//        let number = number % prime;
+//        FpElem { number, prime }
+//    }
+//}
 
+impl<T: GenericUInt> Sub for &FpElem<T> {
+    type Output = FpElem<T>;
+    fn sub(self, tosub: &FpElem<T>) -> FpElem<T> {
+        assert!(self.prime == tosub.prime, "Prime base must be the same");
+        FpElem {
+            number: sub_mod(self.number, tosub.number, self.prime),
+            prime: self.prime,
+        }
+    }
+}
+
+impl<T: GenericUInt> Add for &FpElem<T> {
+    type Output = FpElem<T>;
+    fn add(self, toadd: &FpElem<T>) -> FpElem<T> {
+        assert!(self.prime == toadd.prime, "Prime base must be the same");
+        FpElem {
+            number: add_mod(self.number, toadd.number, self.prime),
+            prime: self.prime,
+        }
+    }
+}
+
+impl<T: GenericUInt> Mul for &FpElem<T> {
+    type Output = FpElem<T>;
+    fn mul(self, tomul: &FpElem<T>) -> FpElem<T> {
+        assert!(self.prime == tomul.prime, "Prime base must be the same");
+        FpElem {
+            number: mul_mod(self.number, tomul.number, self.prime),
+            prime: self.prime,
+        }
+    }
+}
+
+impl<T: GenericUInt> Mul<T> for &FpElem<T> {
+    type Output = FpElem<T>;
+    fn mul(self, tomul: T) -> FpElem<T> {
+        FpElem {
+            number: mul_mod(self.number, tomul % self.prime, self.prime),
+            prime: self.prime,
+        }
+    }
+}
+
+pub trait Pow<T> {
+    fn pow_mod(&self, exponent: T) -> Self;
+}
+
+impl<T> Pow<T> for FpElem<T>
+where
+    T: GenericUInt,
+{
+    fn pow_mod(&self, exponent: T) -> Self {
+        FpElem {
+            number: pow_mod(self.number, exponent, self.prime),
+            prime: self.prime,
+        }
+    }
+}
+
+impl<T: GenericUInt + Debug> Div for &FpElem<T> {
+    type Output = FpElem<T>;
+    fn div(self, rhs: Self) -> FpElem<T> {
+        FpElem {
+            number: mul_mod(self.number, mod_inv(rhs.number, rhs.prime), rhs.prime),
+            prime: self.prime,
+        }
+    }
+}
--- tests/fptests.rs
+++ tests/fptests.rs
@@ -10,7 +10,75 @@ fn fpelem_new() {
         let result = FpElem::new(inpair.0, inpair.1);
         assert_eq!(
             (result.number, result.prime),
+            *outpair
+        );
+        let result = FpElem::<U512>::new_from(inpair.0, inpair.1);
+        assert_eq!(
+            (result.number, result.prime),
             (U512::from(outpair.0), U512::from(outpair.1))
         );
+    }
+    for (inpair, outpair) in pairs.iter() {
+        let result = FpElem::new(U512::from(inpair.0), U512::from(inpair.1));
+        assert_eq!(
+            (result.number, result.prime),
+            (U512::from(outpair.0), U512::from(outpair.1))
+        );
+    }
+}
+
+
+#[test]
+fn fpelem_add() {
+    // format: ((lhs, rhs, mod), result)
+    let pairs = [((9192, 127712, 65537), 5830), ((1, 9911, 65537), 9912)];
+    for (in3, out) in pairs.iter(){
+        let lhs = FpElem::new(in3.0, in3.2);
+        let rhs = FpElem::new(in3.1, in3.2);
+        let expectedout = FpElem::new(*out, in3.2);
+        assert_eq!(&lhs + &rhs, expectedout);
+    }
+}
+
+#[test]
+fn fpelem_sub() {
+    // format: ((lhs, rhs, mod), result)
+    let pairs = [((9192, 127712, 65537), 12554), ((1, 9911, 65537), 55627)];
+    for (in3, out) in pairs.iter(){
+        let lhs = FpElem::new(in3.0, in3.2);
+        let rhs = FpElem::new(in3.1, in3.2);
+        let expectedout = FpElem::new(*out, in3.2);
+        assert_eq!(&lhs - &rhs, expectedout);
+    }
+}
+#[test]
+fn fpelem_mul() {
+    // format: ((lhs, rhs, mod), result)
+    let pairs = [((9192, 127712, 65537), 29960), ((1, 9911, 65537), 9911)];
+    for (in3, out) in pairs.iter(){
+        let lhs = FpElem::new(in3.0, in3.2);
+        let rhs = FpElem::new(in3.1, in3.2);
+        let expectedout = FpElem::new(*out, in3.2);
+        assert_eq!(&lhs * &rhs, expectedout);
+    }
+}
+
+#[test]
+fn fpelem_div() {
+    // format: ((lhs, rhs, mod), result)
+    let pairs = [((92192, 127712, 65537), 7653), ((1, 9911, 65537), 47941)];
+    for (in3, out) in pairs.iter(){
+        let lhs = FpElem::new(in3.0, in3.2);
+        let rhs = FpElem::new(in3.1, in3.2);
+        let expectedout = FpElem::new(*out, in3.2);
+        assert_eq!(&lhs/&rhs, expectedout);
+    }
+    // for runtime
+    let p = "0xfffffffffffffffffffffffffffffffffffffffffffffffffffffffefffffc2f";
+    let to_invert = [10,99991,2,1208128,1299392];
+    for r in to_invert.iter(){
+        let to_inv = FpElem::<U512>::new_from(*r, p);
+        let one = FpElem::<U512>::new_from(1,p);
+        assert_eq!(&to_inv/&to_inv, one);
     }
 }
